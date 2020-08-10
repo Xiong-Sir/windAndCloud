50244342cefbd5985e5acb5474cbb67be82cfa1e
ZhuXiongfeng <zhuxiongfeng@dhcc.com.cn>
2020-08-10 15:06:33 +0800

    启动项目搭建

---
+++ shopp/src/main/java/com/dhc/shopp/ShoppApplication.java
@@ -0,0 +1,13 @@
+package com.dhc.shopp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppApplication.class, args);
+    }
+
+}
---
+++ shopp/src/test/java/com/dhc/shopp/ShoppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhc.shopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
