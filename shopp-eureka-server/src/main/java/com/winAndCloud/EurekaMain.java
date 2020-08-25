4d99c77cafb0e7fabe6b5253bbe80311d1733f65
ZhuXiongfeng <zhuxiongfeng@dhcc.com.cn>
2020-08-25 16:08:40 +0800

    构建Eureka注册中心

---
+++ shopp-eureka-server/src/main/java/com/winAndCloud/EurekaMain.java
@@ -0,0 +1,17 @@
+package com.winAndCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Xiongfeng
+ * @date 2020/8/25 0025 15:54
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain.class,args);
+    }
+}
