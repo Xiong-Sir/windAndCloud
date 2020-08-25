bc0cea12bba9fe478890a39854bf9c12dccc9caf
ZhuXiongfeng <zhuxiongfeng@dhcc.com.cn>
2020-08-25 15:41:13 +0800

    重构的pay模块

---
+++ Code-Generator/src/test/java/com/winandcloud/codegenerator/CodegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winandcloud.codegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodegeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shopp-pay/src/main/java/com/windAndCloud/ShoppPay.java
@@ -0,0 +1,16 @@
+package com.windAndCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xiongfeng
+ * @date 2020/8/25 0025 14:35
+ */
+
+@SpringBootApplication
+public class ShoppPay {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppPay.class,args);
+    }
+}
